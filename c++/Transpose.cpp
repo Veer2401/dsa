b934a31933b31835413e49a2688ac087a1755281
Veer Harischandrakar <1012411025@despu.edu.in>
2025-11-11 12:29:56 +0530

    Update and rename SimpleTranspose.cpp to Transpose.cpp

--- c++/Transpose.cpp
+++ c++/Transpose.cpp
@@ -3,18 +3,20 @@ using namespace std;
 
 int main(){
     int r,c;
-    cout << "Enter rows and columns: ";
-    cin >> r >> c;
+    cout << "Enter rows: ";
+    cin >> r;
+    cout << "Enter columns: ";
+    cin >> c;
 
     int a[10][10];
-    cout << "Enter Matrix Elements: " << endl;
+    cout << "Enter matrix elements:\n";
     for(int i=0;i<r;i++){
         for(int j=0;j<c;j++){
             cin >> a[i][j];
         }
     }
 
-    int compact[20][3], k = 0;
+    int compact[20][3], k=1;
     for(int i=0;i<r;i++){
         for(int j=0;j<c;j++){
             if(a[i][j] != 0){
@@ -30,32 +32,33 @@ int main(){
     compact[0][1] = c;
     compact[0][2] = k-1;
 
-    cout << "Compact matrix: \n";
-    cout << "Row Col Val\n";
+    cout << "Compact matrix:\n ";
+    cout << "ROW COL VAL\n";
     for(int i=0;i<k;i++){
         cout << compact[i][0] << " " << compact[i][1] << " " << compact[i][2] << endl;
     }
 
-    int simple[20][3], t = 0;
-    for(int col=0;col < c; col ++){
-        for(int i=0;i<k;i++){
+    int transpose[20][3],t=1;
+    for(int col=0;col<c;col++){
+        for(int i=1;i<k;i++){
             if(compact[i][1] == col){
-                simple[t][0] = compact[i][1];
-                simple[t][1] = compact[i][0];
-                simple[t][2] = compact[i][2];
+                transpose[t][0] = compact[i][1];
+                transpose[t][1] = compact[i][0];
+                transpose[t][2] = compact[i][2];
                 t++;
             }
         }
     }
 
-    cout << "Simple Transpose: \n";
-    cout << "Row Col Val\n";
+    transpose[0][0] = compact[0][1];
+    transpose[0][1] = compact[0][0];
+    transpose[0][2] = compact[0][2];
+
+    cout << "Transpose matrix:\n";
+    cout << "ROW COL VAL\n";
     for(int i=0;i<t;i++){
-        cout << simple[i][0] << " " << simple[i][1] << " " << simple[i][2] << endl;
+        cout << transpose[i][0] << " " << transpose[i][1] << " " << transpose[i][2] << endl;
     }
+
+    return 0;
 }
-
-
-
-
-
