8d288a62a9ce4416531537a5c2a6aeff8cec2525
Veer Harischandrakar <1012411025@despu.edu.in>
2026-09-07 16:16:03 +0530

    Update Recursive.cpp

--- Design and Analysis of Algorithm/Recursive.cpp
+++ Design and Analysis of Algorithm/Recursive.cpp
@@ -43,38 +43,21 @@ int binarySearch(int arr[], int low, int high, int key)
 void merge(int arr[], int low, int mid, int high)
 {
     int temp[100];
-    int i = low;
-    int j = mid + 1;
-    int k = low;
+    int i = low, j = mid + 1, k = low;
 
     while (i <= mid && j <= high)
     {
-        if (arr[i] <= arr[j])
-        {
-            temp[k] = arr[i];
-            i++;
-        }
+        if (arr[i] < arr[j])
+            temp[k++] = arr[i++];
         else
-        {
-            temp[k] = arr[j];
-            j++;
-        }
-        k++;
+            temp[k++] = arr[j++];
     }
 
     while (i <= mid)
-    {
-        temp[k] = arr[i];
-        i++;
-        k++;
-    }
+        temp[k++] = arr[i++];
 
     while (j <= high)
-    {
-        temp[k] = arr[j];
-        j++;
-        k++;
-    }
+        temp[k++] = arr[j++];
 
     for (i = low; i <= high; i++)
         arr[i] = temp[i];
@@ -92,7 +75,6 @@ void mergeSort(int arr[], int low, int high)
 
     merge(arr, low, mid, high);
 }
-
 
 // (c) Sum of First N Factorials
 
